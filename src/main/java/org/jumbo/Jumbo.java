2911fba91c485a90f3312a7ac1ffb053f595db99
raslei <rasmus.leijon@gmail.com>
2020-07-08 17:52:38 +0200

    Added object

--- src/main/java/org/jumbo/Jumbo.java
+++ src/main/java/org/jumbo/Jumbo.java
@@ -7,9 +7,14 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import org.jumbo.codec.Codec;
+import org.jumbo.codec.CodecRegistry;
 
 public class Jumbo {
 	private final JumboJni jni = new JumboJni();
+	private final CodecRegistry registry;
 	
 	private Jumbo(int size) throws IOException {
 		String libName = "libjumbo_jni.so"; // The name of the file in resources/ dir
@@ -22,6 +27,7 @@ public class Jumbo {
 		    Files.copy(in, nativeLibTmpFile.toPath());
 		}
 		System.load(nativeLibTmpFile.getAbsolutePath());
+		registry = CodecRegistry.get();
 		jni.init(size);
 	}
 	
@@ -33,6 +39,45 @@ public class Jumbo {
 		}
 	}
 
+	public void putObject(int table, Object key, Object value) throws IOException {
+		Codec<Object, Object> val = registry.getCodec(table);
+		if (val == null) {
+			throw new RuntimeException("Could not find a codec registered for table " + table);
+		}
+		put(table, val.serializeKey(key), val.serializeValue(value));
+	}
+	
+	public Object getObject(int table, Object key) throws IOException {
+		Codec<Object, Object> val = registry.getCodec(table);
+		if (val == null) {
+			throw new RuntimeException("Could not find a codec registered for table " + table);
+		}
+		byte[] result = get(table, val.serializeKey(key));
+		if (result.length == 0)
+			return null;
+		return val.deserializeValue(result);
+	}
+	
+	public List<Object> getKeysObject(int table, int limit) throws IOException {
+		Codec<Object, Object> val = registry.getCodec(table);
+		if (val == null) {
+			throw new RuntimeException("Could not find a codec registered for table " + table);
+		}
+		List<byte[]> result = keys(table, limit);
+		return result
+			.stream()
+			.map(val::deserializeKey)
+			.collect(Collectors.toList());
+	}
+
+	public void deleteObject(int table, Object key) throws IOException {
+		Codec<Object, Object> val = registry.getCodec(table);
+		if (val == null) {
+			throw new RuntimeException("Could not find a codec registered for table " + table);
+		}
+		del(table, val.serializeKey(key));
+	}
+	
 	public void put(int table, byte[] key, byte[] value) {
 		jni.put(table, key, value);
 	}
