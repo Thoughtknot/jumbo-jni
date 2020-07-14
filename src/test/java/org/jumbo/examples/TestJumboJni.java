03564a74afcf274aed1f0d46c537cc54230a3670
raslei <rasmus.leijon@gmail.com>
2020-07-14 19:23:19 +0200

    Rewrote test as actual test.

--- src/test/java/org/jumbo/examples/TestJumboJni.java
+++ src/test/java/org/jumbo/examples/TestJumboJni.java
@@ -1,19 +1,22 @@
 package org.jumbo.examples;
 
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 
 import org.jumbo.Jumbo;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class TestJumboJni {
 
-	public static void main(String[] args) {
+	@Test
+	public void test() {
+		System.out.println("Starting test");
 		Jumbo jumbo = Jumbo.initialize(1024);
+		System.out.println("Jumbo initialized!");
 		{
 			jumbo.put(99, "a".getBytes(), "apa".getBytes());
 			String val = new String(jumbo.get(99, "a".getBytes()));
-			System.out.println(val);
+			System.out.println("val" + val);
 		}
 		jumbo.put(99, "b".getBytes(), "test".getBytes());
 		{
@@ -36,10 +39,11 @@ public class TestJumboJni {
 		jumbo.put(99, "2".getBytes(), "c".getBytes());
 		jumbo.put(99, "99".getBytes(), "a".getBytes());
 		jumbo.put(99, "110".getBytes(), "b".getBytes());
-		byte[] val = jumbo.get(99, "99".getBytes());
-		System.out.println(new String(val));
+		byte[] val99 = jumbo.get(99, "99".getBytes());
+		byte[] val110 = jumbo.get(99, "110".getBytes());
 		
-		System.out.println(new String(jumbo.get(99, String.valueOf(110).getBytes())));
+		Assert.assertEquals("b", val110);
+		Assert.assertEquals("a", val99);
 	}
 	
 	static void foo(Object a) {
